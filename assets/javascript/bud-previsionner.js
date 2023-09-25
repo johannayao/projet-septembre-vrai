86ac51e152e9820256bfe97ac6b7db86c4bf66cc
johanna <yaojohanna@2023.com>
2023-09-25 21:52:13 +0000

    mon commit ouff

--- assets/javascript/bud-previsionner.js
+++ assets/javascript/bud-previsionner.js
@@ -120,31 +120,41 @@ formulaire.addEventListener("submit",(e)=>{
 form = document.querySelector("form")
 form.addEventListener("submit", (e)=>{
     e.preventDefault
-    let egale= renduJournee.value+perte.value
+    // let egale= renduJournee.value+perte.value
     console.log(egale);
 })
 bouton.addEventListener("click" , (e)=>{
+  e.preventDefault();
   const prixAchat = document.querySelector("#prixAchat");
   const  perte= document.querySelector("#perte");
   const prixProduit = document.querySelector("#prixProduit");
-//   const benefice = document.querySelector("#benefice");
-//   const eppargne = document.querySelector("#eppargne");
+  const date = document.querySelector("#date");
+  
+
+  //const benefice = document.querySelector("#benefice");
+  const eppargne = document.querySelector("#eppargne");
   const renduJournee = document.querySelector("#renduJournee");
   
    
   let information ={
+    date:parseInt(date.value),
     prixAchat: parseInt(prixAchat.value),
     perte : parseInt(perte.value),
     prixProduit: parseInt(prixProduit.value),
+    
+    
     // benefice:  parseInt(benefice.value),
-    // eppargne: eppargne.value,
+    eppargne: parseInt(eppargne.value),
     renduJournee : parseInt(renduJournee.value)
   }
-    let upBoutique = localStorage.getItem("boutique")
+
+  console.log(information);
+
+     let upBoutique = localStorage.getItem("boutique")
     upBoutique = JSON.parse(upBoutique)
     console.log(upBoutique)
 
-  if(upBoutique != null){
+ if(upBoutique == null){
     information.id = upBoutique.length+1
     upBoutique.push( information)
      localStorage.setItem ("boutique",JSON.stringify(upBoutique))
@@ -156,32 +166,37 @@ bouton.addEventListener("click" , (e)=>{
         upBoutique.push(information)
         localStorage.setItem("boutique", JSON.stringify(upBoutique))
          window.location.reload();
-    }
+    } 
 })
 
 
 
-function recupBoutique (table, len){
+function recupBoutique (table){
     let tbody = document.querySelector(".tbody");
     table.forEach(boutique => {
-        console.log( (boutique.prixAchat - (boutique.renduJournee + boutique.perte)) );
+        // console.log( (boutique.prixAchat - (boutique.renduJournee + boutique.perte)) );
+        console.log(boutique);
         let tout = `<tr id="${boutique.id}">
             
-            <td data-title="prixAchat">${boutique.prixAchat} ${"Fcfa"}</td>
-            <td data-title="perte">${boutique.perte} ${"Fcfa"}</td>
+             
+        <td data-title="prixAchat">${boutique.prixAchat} ${"Fcfa"}</td>
+            
+          
+             <td data-title="benefice">${boutique.eppargne} ${"Fcfa"}</td>
+            
+             <td data-title="eppargne"> ${boutique.benefice}</td>
             <td data-title="prixProduit">${boutique.prixProduit}  ${"Fcfa"}</td>
-            <td data-title="benefice">${(boutique.prixAchat - (boutique.renduJournee + boutique.perte))} ${"Fcfa"}</td>
-            <td data-title="eppargne">${boutique.value/2} ${"Fcfa"}</td>
-            <td data-title="renduJournee">${boutique.renduJournee} ${"Fcfa"}</td>
-            <td data-title="MODIFICATION" ><div class="chance"><i class="edite" id="delete-${boutique.id}">Modifier</i> <i class="delete" onclick="deleteItem(event)" id="delete-${boutique.id}">Supprimer</i></div></td>
+            <td data-title="date">${boutique.date}</td>
+             <td data-title="renduJournee">${boutique.renduJournee} ${"Fcfa"}</td>
+             <td data-title="MODIFICATION" ><div class="chance"><i class="edite" id="delete-${boutique.id}">Modifier</i> <i class="delete" onclick="deleteItem(event)" id="delete-${boutique.id}">Supprimer</i></div></td>
         </tr>`;
         tbody.innerHTML += tout;
         // console.log(prixProduit);
     });
 }
 
-let allboutique=JSON.parse(localStorage.getItem("boutique")) || []
-// console.log(allboutique)
+let allboutique=JSON.parse(localStorage.getItem("boutique"))
+console.log(allboutique)
 recupBoutique(allboutique, allboutique?.length)
 
 function deleteItem(e){
