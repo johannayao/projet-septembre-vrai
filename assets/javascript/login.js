91e58a728258a9fc674034c54b49e4da176e31e6
johanna <yaojohanna@2023.com>
2023-09-26 13:24:13 +0000

    jolie commit

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -14,7 +14,7 @@ form.addEventListener("submit", (e) => {
            
         }
         sessionStorage.setItem("sessionRegistPlus", JSON.stringify(dataSession))
-        window.location.href="https://github.com/johannayao/projet-septembre-vrai/corporates/page2.html";
+        window.location.href="https://https://johannayao.github.io/projet-septembre-vrai/";
         //   window.location.href="../../corporates/page2.html";
         
         console.log(isUser);
